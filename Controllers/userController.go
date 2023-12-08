14abd82ca276c7b32c0bcbb5c6b5fd485fdcaf2b
rafsnil <rafsnil@gmail.com>
2023-12-08 15:35:10 +0600

    Worked on Helper and Controller

--- Controllers/userController.go
+++ Controllers/userController.go
@@ -1,1 +1,41 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	database "github.com/rafsnil/Go-JWT-Authentication/Database"
+	helpers "github.com/rafsnil/Go-JWT-Authentication/Helpers"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "User")
+var validate = validator.New()
+
+func HashPassword()
+
+func VerifyPassword()
+
+func ExecuteSignUp()
+
+func ExecuteLogin()
+
+func GetAllUsers()
+
+// GET USER BY ID HANDLER
+// This can be accessed by only ADMINS
+func GetUserByID() gin.HandlerFunc {
+	return func(requestCntxt *gin.Context) {
+		//Getting the userId from the url parameter
+		//Do NOT USE PARAMS, it is different function.
+		userId := requestCntxt.Param("user_id")
+
+		//Checking if the user type, if its an admin
+		err := helpers.MatchUserTypeToUid(requestCntxt, userId)
+		if err != nil {
+			requestCntxt.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+			return
+		}
+	}
+}
--- Helpers/authHelper.go
+++ Helpers/authHelper.go
@@ -1,1 +1,39 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(requestCntxt *gin.Context, role string) (err error) {
+	userType := requestCntxt.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("unauthorized to access this resource❗")
+	}
+	return err
+}
+
+func MatchUserTypeToUid(requestCntxt *gin.Context, userId string) (err error) {
+	//Getting the client's user type
+	userType := requestCntxt.GetString("user_type")
+	//This uid is in the gin.Context through the Authenticate() middleware
+	//"uid" is the user id of the person who is sending the request
+	uid := requestCntxt.GetString("uid")
+	err = nil
+
+	//Checking if the client is user type 'USER' and if the client's uid
+	//is not equal to the 'user_id' the client is trying to get
+
+	//If the client is USER and the client's uid is not equal to the userId he is looing for
+	//then show an error
+	if userType == "USER" && uid != userId {
+		err = errors.New("unauthorized to access this resource❗")
+		return err
+	}
+
+	// err = CheckUserType(requestCntxt, userType)
+
+	return err
+}
